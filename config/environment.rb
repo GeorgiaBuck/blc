3e0462616bc7cfc3310b1e62e428e3c27eb46046
GeorgiaBuck <bucklandlo@bellsouth.net>
2011-12-03 00:45:10 -0500

    changed database.yml

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,5 @@ ENV['RAILS_ENV'] ||='production'
 
 # Initialize the rails application
 Blc::Application.initialize!
-
+config.action_controller.session_store = :active_record_store
+rake create_sessions_table
